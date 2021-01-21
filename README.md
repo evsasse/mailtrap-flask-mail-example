f9354c3ef9f5ac124c1d939cb603cf3967bfe862
Evandro Sasse <evsasse@gmail.com>
2021-01-21 07:53:00 -0300

    Sync email

--- README.md
+++ README.md
@@ -4,5 +4,6 @@ Example on how to use Flask-Mail with Mailtrap
 
 ## Instruction
 
+- Create a `.env` file, based on `.env.example`, with your Mailtrap details
 - `docker-compose up --build``
 - localhost:5000
--- main.py
+++ main.py
@@ -1,6 +1,25 @@
+import os
+
 from flask import Flask
+from flask_mail import Mail, Message
+
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+app.config['MAIL_SERVER']='smtp.mailtrap.io'
+app.config['MAIL_PORT'] = 2525
+app.config['MAIL_USERNAME'] = os.environ['MAIL_USERNAME']
+app.config['MAIL_PASSWORD'] = os.environ['MAIL_PASSWORD']
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USE_SSL'] = False
+
+mail = Mail(app)
+
+@app.route("/")
+def index():
+  msg = Message('Hello from the other side!', sender='peter@mailtrap.io', recipients=['paul@mailtrap.io'])
+  msg.body = "Hey Paul, sending you this email from my Flask app, lmk if it works"
+  mail.send(msg)
+  return "Message sent!"
+
+if __name__ == '__main__':
+   app.run(debug = True)
